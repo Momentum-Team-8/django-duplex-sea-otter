a1f3c9e9695aa2a2cc9c6c15016e384167b69076
shaunedaniel <75916901+shaunedaniel@users.noreply.github.com>
2021-06-25 20:58:57 -0400

    delete function

--- django_snippet/urls.py
+++ django_snippet/urls.py
@@ -15,11 +15,11 @@ Including another URLconf
 """
 from django.conf import settings
 from django.contrib import admin
-from django.urls import include,path
+from django.urls import include, path
+from snippets import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('collection/<int:pk>/delete',
+         views.delete_snippets, name='delete_snippets'),
 ]
-
-
-
--- snippets/views.py
+++ snippets/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from snippets.models import Snippet
+from django.shortcuts import render, redirect, get_object_or_404
 
 # Create your views here.
+
+
+def delete_snippets(request, pk):
+    snippet = get_object_or_404(Snippet, pk=pk)
+    if request.method == 'POST':
+        snippet.delete()
+        return redirect(to='list_snippets')
+
+    return render(request, "snippets/delete_snippets.html",
+                  {"snippet": snippet})
